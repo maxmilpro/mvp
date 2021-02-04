a1bd3f753e5eb8f88bb699215a450a1193a3de71
Max Proano <proanom89@gmail.com>
2021-02-03 18:21:20 -0800

    Inserted record into db with asyn function

--- db/connect.js
+++ db/connect.js
@@ -2,14 +2,24 @@ const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'recipeBook';
-let db;
 
-MongoClient.connect(url, function(err, client) {
-  if (err) return console.log(err);
+const client = new MongoClient(url);
 
-  db = client.db(dbName);
-  console.log(`Connected MongoDB: ${url}`);
-  console.log(`Database: ${dbName}`)
-})
+async function run() {
+  try {
+    await client.connect();
 
-// module.exports.client = new MongoClient(uri);
+    const database = client.db(dbName);
+    const collection = database.collection('ingredients');
+    const doc = { name: 'chicken', category: 'protein' };
+    const result = await collection.insertOne(doc);
+
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`
+    );
+  } finally {
+    await client.close();
+  }
+}
+
+// run().catch(console.dir);
