ff90f08c8f2b3a42a1e463ab7cecc047fa1074b1
Max Proano <proanom89@gmail.com>
2021-02-05 14:35:00 -0800

    Dynamically render components based on number of ingredients

--- src/Ingredients.js
+++ src/Ingredients.js
@@ -2,16 +2,15 @@ import React, {Component} from "react";
 import {hot} from "react-hot-loader";
 import Ingredient from "./Ingredient.js";
 
-function Ingredients(props) {
-  var ingredients = props.ingredients;
+function Ingredients({ingredients}) {
   return (
     <div>
-      {/* {ingredients.map((item) =>
-        <Ingredient ingredient={item}/>
-      )} */}
-      <Ingredient ingredient={ingredients[0]}/>
+      {ingredients.map(function(item) {
+        return <Ingredient key={item.name} ingredient={item}/>
+      })}
+      {/* <Ingredient ingredient={ingredients[0]}/>
       <Ingredient ingredient={ingredients[1]}/>
-      <Ingredient ingredient={ingredients[2]}/>
+      <Ingredient ingredient={ingredients[2]}/> */}
     </div>
   )
 }
--- src/sample-data/sample-ingredients.js
+++ src/sample-data/sample-ingredients.js
@@ -21,7 +21,16 @@ var sampleIngredients = [
     "quantity": 1,
     "category": "vegetable",
     "meal": "lunch"
+  },
+  {
+    "_id": "601caf506cdf41028b482030",
+    "name": "apple",
+    "quantity": 3,
+    "category": "fruit",
+    "meal": "snack"
   }
 ];
 
-export default hot(module)(sampleIngredients);
+// export default hot(module)(sampleIngredients);
+
+export default sampleIngredients;
